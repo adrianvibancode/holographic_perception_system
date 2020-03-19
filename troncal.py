c889d2938f8d27e276d4ad974a780ebe63571d99
adrianvibancorosales <adrianvibanco@outlook.com>
2020-03-19 10:50:59 -0600

    Insertion nodes

---
+++ troncal.py
@@ -0,0 +1,16 @@
+
+""" import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+msg = "hello world" """
+
+#Production
+node={1:{1:{1:"3"}} , 2:"4"}
+node2=[[2,3,4,5],[3,3,3,3]]
+
+print(node2[1][1])
+
